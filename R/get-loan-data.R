325e47d1da385e246578e6d1f99e76d58fc22cef
dantonnoriega <danton.noriega@gmail.com>
2017-10-24 11:58:05 -0400

    updated data. working loan rate return function. what is rng_max = 1 mean?

--- R/get-loan-data.R
+++ R/get-loan-data.R
@@ -2,21 +2,71 @@
 #' @importFrom magrittr %>%
 #' @param x path to csv file.
 
-get_loan_data <- function() {
+get_loan_data <- function(import = FALSE) {
 
-  x <- "data-raw/Market Loan Terms MASTER.csv"
+  if(import) {
+    x <- "data-raw/market-loan-terms-master.csv"
 
-  dat <- readr::read_csv(x) %>%
-    dplyr::mutate_if(is.character, dplyr::funs(gsub('\\$|,|%|-', '', .))) %>%
-    dplyr::select(-state, -dplyr::contains('modelyear')) %>%
-    dplyr::select(name:program, LTV_index, dplyr::everything()) %>%
-    dplyr::mutate_at(5:55, as.numeric) %>%
-    dplyr::select(-dplyr::contains('loan_amt'), -warranty_max) %>%
-    dplyr::mutate(name = iconv(name, "latin1", "ASCII", sub = " ")) %>%
-    dplyr::mutate(name = gsub("[ ]+", " ", name))
+    # read and clean data
+    dat <- suppressMessages(readr::read_csv(x)) %>%
+      dplyr::mutate_if(is.character, dplyr::funs(gsub('\\$|,|%|-', '', .))) %>%
+      dplyr::select(-state, -dplyr::contains('modelyear')) %>%
+      dplyr::select(name:program, LTV_index, dplyr::everything()) %>%
+      dplyr::mutate_at(5:55, as.numeric) %>% # convert select cols to numeric
+      dplyr::select(-dplyr::contains('loan_amt'), -warranty_max, -minrisk, -maxrisk) %>%
+      dplyr::mutate(name = iconv(name, "latin1", "ASCII", sub = " ")) %>%
+      dplyr::mutate(name = gsub("[ ]+", " ", name))
 
-  dat <- dat %>%
-    tidyr::gather(key = 'rate_name', value = 'rate', Aplus:M_rng_min) %>%
-    dplyr::filter(!is.na(rate))
+    loan_data <- dat %>%
+      tidyr::gather(key = 'rate_name', value = 'rate', Aplus, A, B, C, D, E, F, G, H, I, J, K, L, M) %>%
+      dplyr::filter(!is.na(rate)) %>%
+      dplyr::filter(!is.na(name)) %>%
+      dplyr::select(name:program, dplyr::contains('LTV_'), months_max, bank_fee, markup, dplyr::everything())
+
+
+    # find and rename ranges by rating name structure
+    rn <- c("Aplus", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M")
+
+    hold <- lapply(rn, function(k) {
+      # pull out shared columns and then only those starting with rating name (e.g. "Aplus_rng_max")
+      k_ <- paste0(k, "_") # add underscore so we can distinguish Aplus from A
+      tmp <- loan_data %>%
+        dplyr::filter(rate_name == k) %>%
+        dplyr::select(name:markup, rate_name, rate, dplyr::starts_with(k_))
+
+      # extract names, remove rating e.g. "Aplus_rng_min" -> "rng_min"
+      nms <- names(tmp) %>%
+        gsub(paste0('^', k_), '', .)
+
+      # return
+      tmp %>%
+        setNames(nms)
+    })
+
+    loan_data <- hold %>%
+      dplyr::bind_rows(.) %>%
+      dplyr::select(name:rate, rng_min, rng_max)
+
+    devtools::use_data(loan_data, overwrite = TRUE)
+
+    return(loan_data)
+
+  } else loan_data
+
+}
+
+
+#' input a score value and extracts
+#' @param s score value
+get_rates <- function(s) {
+
+  dat <- get_loan_data()
+
+  dat %>%
+    dplyr::rowwise() %>%
+    dplyr::mutate(keep = dplyr::between(s, rng_min, rng_max)) %>%
+    dplyr::filter(keep == TRUE) %>%
+    dplyr::select(-keep) %>%
+    dplyr::pull(rate)
 
 }
